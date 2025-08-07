1c85356129b6c70cb9d579f7bb84035712c23145
Dev Dhanuka <lohandanuka@gmail.com>
2025-08-07 11:54:03 +0530

    navbar ajusted

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 w-full z-50 bg-white dark:bg-gray-900 shadow">
+      <nav className="fixed top-0 w-full z-50 bg-white dark:bg-gray-900 ">
         <div className="max-w-7xl mx-auto px-6 sm:px-12 py-4 flex justify-between items-center">
           {/* Logo */}
           <div className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 text-transparent bg-clip-text">
